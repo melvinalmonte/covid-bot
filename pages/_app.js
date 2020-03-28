5eb5dc4c6ca2aa163a49a7595fd3bcb00dd79bd9
Melvin <melvinalmonte@gmail.com>
2020-03-28 18:22:10 -0400

    added bulma css

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles.scss"
+// This default export is required in a new `pages/_app.js` file.
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
